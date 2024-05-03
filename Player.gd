cfb5dc991a06285c27aba88a68da3e5e80c64a62
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-05-02 21:11:42 -0500

    completed both audio lessons

--- Player.gd
+++ Player.gd
@@ -4,12 +4,20 @@ extends RigidBody3D
 @export_range(750.0, 3000.0) var thrust: float = 1000.0
 @export var torque_thrust: float = 100.0
 
+@onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
+@onready var success_audio: AudioStreamPlayer = $SuccessAudio
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+
 var is_transitioning: bool = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * thrust)
+		if !rocket_audio.playing:
+			rocket_audio.play()
+	else:
+		rocket_audio.stop()
 		
 	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
@@ -27,18 +35,20 @@ func _on_body_entered(body: Node) -> void:
 
 func crash_sequence() -> void:
 	print("KABOOM!")
+	explosion_audio.play()
 	set_process(false)
 	is_transitioning = true
 	var tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(2.5)
 	tween.tween_callback(get_tree().reload_current_scene)
 
 func complete_level(next_level_file: String) -> void:
 	print("Level Complete")
+	success_audio.play()
 	set_process(false)
 	is_transitioning = true
 	var tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(2.5)
 	tween.tween_callback(
 		get_tree().change_scene_to_file.bind(next_level_file)
 	)
